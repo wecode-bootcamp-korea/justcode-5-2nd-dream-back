f1f4df6508e664a5c108367746428b5ab54efe89
KIMYECHAN <kyc5244@gmail.com>
2022-07-11 16:39:41 +0900

    프로젝트 백엔드 초기 세팅

---
+++ README.md
@@ -0,0 +1,6 @@
+# 실행 순서
+
+1. 프로젝트 클론
+2. npm i
+3. npx prisma generate
+4. npx db pull
---
+++ server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const http = require("http");
+const cors = require("cors");
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const routes = require("./routes");
+const prisma = new PrismaClient();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+// app.use(routes);
+
+////////// TEST ///////////////
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
+///////////////////////////////
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 10010;
+
+const start = async () => {
+  // 서버를 시작하는 함수입니다.
+  try {
+    server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+  } catch (err) {
+    console.error(err);
+    await prisma.$disconnect(); // 에러가 발생했을 시에 database 연결을 종료합니다.
+  }
+};
+
+start();
