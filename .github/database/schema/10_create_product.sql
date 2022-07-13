6f1259f4754b1ec2c214ad1b2d4b5ded913ded57
Sujin Choi <hissujinc@gmail.com>
2022-07-13 21:01:50 +0900

    Add: Schema 파일 추가

---
+++ .github/database/schema/10_create_product.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `product` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `comment` varchar(255),
+  `model_number` varchar(255),
+  `color` varchar(255),
+  `sex` varchar(255),
+  `category_id` int,
+  `sub_category_id` int,
+  `brand_id` int,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY fk_product_brand_id_brand_id (brand_id) REFERENCES brand (id) ON DELETE CASCADE,
+  FOREIGN KEY fk_product_category_id_category_id (category_id) REFERENCES category (id) ON DELETE CASCADE,
+  FOREIGN KEY fk_product_sub_category_id_sub_category_id (sub_category_id) REFERENCES sub_category (id) ON DELETE CASCADE
+);
