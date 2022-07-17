3e416daba8edcea259d08a20b0ff96fc386be274
KIMYECHAN <kyc5244@gmail.com>
2022-07-18 01:17:24 +0900

    MODIFIY : mainPage API 수정

--- controllers/mainPage.js
+++ controllers/mainPage.js
@@ -3,6 +3,9 @@ const { displayInfo } = require('../services/maPage');
 const mainPageController = async (req, res, next) => {
   try {
     const [justDropList, popularList] = await displayInfo();
+    for (let i = 0; i < popularList.length; i++) {
+      popularList[i].sell_num = parseInt(popularList[i].sell_num);
+    }
     return res.status(201).json({ data: [justDropList, popularList] });
   } catch (err) {
     next(err);
