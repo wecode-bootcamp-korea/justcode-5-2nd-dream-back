900a4c7d2d90eec57da0ed9d8635ec98339bc02e
최정훈 <jeongssi94@gmail.com>
2022-07-19 19:57:40 +0900

    FIX : sale API  에러수정 완료

--- models/sale.js
+++ models/sale.js
@@ -1,15 +1,10 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-async function postsale(
-  price,
-  user_id,
-  product_detail_id,
-  product_id,
-  sell_status_id
-) {
+async function postsale(price, user_id, product_detail_id, product_id) {
   const dbsale = await prisma.$queryRaw`
-  INSERT INTO sell(price,user_id,product_detail_id,product_id,sell_status_id) values (${price},${user_id},${product_detail_id},${product_id},${sell_status_id}) 
+  INSERT INTO sell(price,user_id,product_detail_id,product_id,sell_status_id) 
+  values (${price},${user_id},${product_detail_id},${product_id},1) 
   `;
   return dbsale;
 }
--- services/sale.js
+++ services/sale.js
@@ -1,19 +1,11 @@
 const salemodels = require('../models/sale');
 
-async function postsale(
-  price,
-  user_id,
-  product_detail_id,
-  product_id,
-  sell_status_id
-) {
-  console.log(price, user_id, 's', 's');
+async function postsale(price, user_id, product_detail_id, product_id) {
   return await salemodels.postsale(
     price,
     user_id,
     product_detail_id,
-    product_id,
-    sell_status_id
+    product_id
   );
 }
 
