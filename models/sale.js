ea24eff2c6da8797d89dbc728881b00c0f74daf6
최정훈 <jeongssi94@gmail.com>
2022-07-19 13:00:37 +0900

    Add: 판매버튼 임시저장

--- models/sale.js
+++ models/sale.js
@@ -1,60 +1,20 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-async function getWishListByUserId(userid) {
-  console.log('getWishList');
-  const dbWishList = await prisma.$queryRaw`
-
-  SELECT
-    r.id,
-    r.price,
-    r.address,
-    r.name,
-    r.description,
-    p.file_url
-
-  FROM room as r
+async function postsale(
+  price,
+  user_id,
+  product_detail_id,
+  product_id,
+  sell_status_id
+) {
+  const dbsale = await prisma.$queryRaw`
+  INSERT INTO sell(price,user_id,product_detail_id,product_id,sell_status_id) 
   
-  LEFT JOIN wishlist on wishlist.room_id = r.id
-  JOIN(
-	SELECT MAX(photo.id) AS photo_id,room_id, MAX(file_url) as file_url
-
-	FROM photo
-
-	GROUP BY room_id
-) 	AS p ON r.id = p.room_id
+  values (${price},${user_id}${product_detail_id},${product_id},${sell_status_id}) 
   
-WHERE wishlist.user_id = ${userid}
 
   `;
-  // console.log('test: ', dbWishList);
-  // return res.status(200).json({ message: 'CREATED' });
-  return dbWishList;
+  return dbsale;
 }
-// export default getUsers;
-
-async function DeletWishList(id, room_id) {
-  // console.log('getWishList', DeletWishList);
-  const dbWishList = await prisma.$queryRaw`
-  DELETE FROM wishlist WHERE user_id = ${id} and room_id = ${room_id}
- 
-  
-  `;
-  //  DELETE FROM wishlist WHERE user_id = ${id} and ${room_id}
-  console.log('Delet: ', dbWishList);
-  // return res.status(200).json({ message: 'CREATED' });
-  return dbWishList;
-}
-
-async function InsertWishList(id, room_id) {
-  console.log('getWishList', id, room_id);
-  const dbWishList = await prisma.$queryRaw`
-  INSERT INTO wishlist(user_id,room_id) values(${id},${room_id})
-  
-  `;
-  console.log('test: ', dbWishList);
-  // return res.status(200).json({ message: 'CREATED' });
-  return dbWishList;
-}
-
-module.exports = { getWishListByUserId, DeletWishList, InsertWishList };
+module.exports = { postsale };
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const mainRoutes = require('./mainPage');
 const mypageRouter = require('./mypage');
+const saleRouter = require('./sale');
 
 router.use(mainRoutes);
 router.use(mypageRouter);
+router.use(saleRouter);
+
 module.exports = router;
