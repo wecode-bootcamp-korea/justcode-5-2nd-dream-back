f486a0bf40c36517a2db67f228a6d925f7432059
KIMYECHAN <kyc5244@gmail.com>
2022-07-20 00:59:45 +0900

    mainPage 코드 리팩토링

--- services/mainPage.js
+++ services/mainPage.js
@@ -2,6 +2,9 @@ const { displayMain } = require('../models/mainPage');
 
 const displayInfo = async () => {
   const [justDropList, popularList] = await displayMain();
+  for (let i = 0; i < popularList.length; i++) {
+    popularList[i].sell_num = parseInt(popularList[i].sell_num);
+  }
   return [justDropList, popularList];
 };
 
