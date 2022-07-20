7c8dd7b65f12f9b34c97454c21569c89a29e5747
KIMYECHAN <kyc5244@gmail.com>
2022-07-20 13:55:20 +0900

    FIX: wishlist user 중복 추가 되는 버그 해결

--- models/wishlist.js
+++ models/wishlist.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function getWish(userId) {
-  const wishlist = prisma.$queryRaw`SELECT product.name,brand.brand, product_images.url AS product_image, product_detail.price, size.size
+  const wishlist = prisma.$queryRaw`SELECT product.id AS product_id, product.name,brand.brand, product_images.url AS product_image, product_detail.price, size.size
     FROM wish JOIN product_detail ON wish.product_detail_id = product_detail.id JOIN product ON product_detail.product_id = product.id 
     JOIN brand ON product.brand_id = brand.id JOIN size ON product_detail.size_id = size.id
     JOIN (SELECT product_id,url FROM product_images WHERE position =1) product_images ON product.id = product_images.product_id
@@ -11,7 +11,8 @@ async function getWish(userId) {
 }
 
 async function getWishMain(userId) {
-  const wishs = prisma.$queryRaw`SELECT product_id FROM wish WHERE user_id = ${userId} GROUP BY product_id`;
+  const wishs =
+    await prisma.$queryRaw`SELECT product_id FROM wish WHERE user_id = ${userId} GROUP BY product_id`;
   return wishs;
 }
 
@@ -23,4 +24,16 @@ async function deleteWish(wishDTO) {
   await prisma.$queryRaw`DELETE FROM wish WHERE user_id=${wishDTO.user_id} and product_id=${wishDTO.product_id} and product_detail_id=${wishDTO.product_detail_id}`;
 }
 
-module.exports = { getWish, createWish, deleteWish, getWishMain };
+async function alreadyWishItem(wishDTO) {
+  const wish =
+    await prisma.$queryRaw`SELECT * FROM wish WHERE user_id=${wishDTO.user_id} and product_id=${wishDTO.product_id} and product_detail_id=${wishDTO.product_detail_id}`;
+  return wish;
+}
+
+module.exports = {
+  getWish,
+  createWish,
+  deleteWish,
+  getWishMain,
+  alreadyWishItem,
+};
--- services/wishlist.js
+++ services/wishlist.js
@@ -3,11 +3,18 @@ const {
   deleteWish,
   getWish,
   getWishMain,
+  alreadyWishItem,
 } = require('../models/wishlist');
 
 const createWishService = async (user_id, product_id, product_detail_id) => {
-  const wishDTO = { user_id, product_id, product_detail_id };
-  await createWish(wishDTO);
+  const wish = await alreadyWishItem(user_id, product_id, product_detail_id);
+  if (wish.length === 0) {
+    const wishDTO = { user_id, product_id, product_detail_id };
+    await createWish(wishDTO);
+  } else {
+    const error = new Error('이미 관심상품에 등록된 상품입니다.');
+    throw error;
+  }
 };
 
 const deleteWishService = async (user_id, product_id, product_detail_id) => {
