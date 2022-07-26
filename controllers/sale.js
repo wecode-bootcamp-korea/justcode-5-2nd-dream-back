a027316ee13acef45c2a1e1dd9c1641b180b34d6
최정훈 <jeongssi94@gmail.com>
2022-07-26 12:36:23 +0900

    Refactor : try catch 추가

--- controllers/sale.js
+++ controllers/sale.js
@@ -1,18 +1,21 @@
 const saleServices = require('../services/sale');
 
 const postsale = async (req, res) => {
-  const price = req.body.price;
-  const user_id = req.body.user_id;
-  const product_detail_id = req.body.product_detail_id;
-  const product_id = req.body.product_id;
-
-  const sale = await saleServices.postsale(
-    price,
-    user_id,
-    product_detail_id,
-    product_id
-  );
-  return res.status(200).json({ message: 'CREATED' });
+  try {
+    const price = req.body.price;
+    const user_id = req.body.user_id;
+    const product_detail_id = req.body.product_detail_id;
+    const product_id = req.body.product_id;
+    const sale = await saleServices.postsale(
+      price,
+      user_id,
+      product_detail_id,
+      product_id
+    );
+    return res.status(200).json({ message: 'CREATED' });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = { postsale };
